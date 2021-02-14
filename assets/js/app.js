ebbadc88fd6a2b1c7439a7b2a88ff2514ca812ef
Em Karimifar <em.karimifar@gmail.com>
2021-02-14 00:56:37 -0600

    added click

--- assets/js/app.js
+++ assets/js/app.js
@@ -7,7 +7,7 @@ $(".photo-hov").mouseenter(function(e){
     console.log(scrolled)
     $("#temp-div").css("left", mouseX)
     $("#temp-div").css("top", mouseY+ scrolled)
-    $("#temp-div").html("<img src='"+imgSrc+"'>")
+    $("#temp-div").html("<div class='hov-img-wrap'><img src='"+imgSrc+"'></div>")
 })
 $(".photo-hov").mousemove(function(e){
     var mouseX = e.clientX +2
@@ -17,6 +17,31 @@ $(".photo-hov").mousemove(function(e){
     $("#temp-div").css("top", mouseY+ scrolled)
 })
 
+$(".photo-hov").click(function(e){
+    $('.photo-hov').unbind('mouseleave');
+    $("#temp-div").addClass("fullscreen")
+    $("#temp-div").css("left", 0)
+    $("#temp-div").css("top", 0)
+
+})
+
+
+$("#temp-div").click(function(e){   
+    console.log("yo")
+    if (document.getElementsByClassName("hov-img-wrap")[0].contains(e.target)){
+      // Clicked in box
+    } else{
+        console.log("cancel")
+        $("#temp-div").removeClass("fullscreen")
+        $("#temp-div").empty();
+
+        //has to be function
+        $('.mouseOn').bind('mouseleave');
+    }
+});
+
+
 $(".mouseOn").mouseleave(function(){
     $("#temp-div").empty();
 })
+
