0344cc721ef6e46fbf81928141f5655b3b43e3aa
Karimifar <em.karimifar@gmail.com>
2021-02-13 23:34:21 -0600

    fixed scroll

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,15 +3,18 @@ $(".photo-hov").mouseenter(function(e){
     var imgSrc = "./assets/img/" + imageName;
     var mouseX = e.clientX +2
     var mouseY = e.clientY +15
+    var scrolled = window.pageYOffset
+    console.log(scrolled)
     $("#temp-div").css("left", mouseX)
-    $("#temp-div").css("top", mouseY)
+    $("#temp-div").css("top", mouseY+ scrolled)
     $("#temp-div").html("<img src='"+imgSrc+"'>")
 })
 $(".photo-hov").mousemove(function(e){
     var mouseX = e.clientX +2
     var mouseY = e.clientY +15
+    var scrolled = window.pageYOffset
     $("#temp-div").css("left", mouseX)
-    $("#temp-div").css("top", mouseY)
+    $("#temp-div").css("top", mouseY+ scrolled)
 })
 
 $(".mouseOn").mouseleave(function(){
